c67e5deb8370b294315b93942ec08f5b575eb225
Oscar Lönnqvist <lonnqvistoscar@gmail.com>
2020-04-09 20:59:16 +0200

    Skifte v1

---
+++ scripts/typewriter.js
@@ -0,0 +1,11 @@
+var i = 0;
+var txt = 'I år och dagar har ni väntat men nu är snart väntan över!';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("welcome").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
